bb6f275179935b904880a975e7d2cd217ab4c69c
noel1984 <44346576+noel1984@users.noreply.github.com>
2020-02-06 09:20:38 -0800

    FEWD 2 A 1

---
+++ JS/scripts.js
@@ -0,0 +1,131 @@
+// Step 1
+/*
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;*/
+
+// Step 2
+
+/*5 //Numeric literal
+    "The White Dog" //String literal
+False //Boolean literal
+null //Null literal*/
+
+//Step 3
+/*let complexExpression = 5 * 10 + 8;
+let complexExpression2 = a + "and" + b;*/
+
+//Step 4
+
+/*let: firstName, lastName, address, city, state, zipCode, yourAge, referralSource, mayWeContactYou;*/
+
+//Step 5
+/*
+firstName = window.prompt("Please enter your first name");
+mayWeContactYou = true;
+lastName = "Nouwels";*/
+
+//Step 6
+/*let a = 5 * "5";
+console.log(a);*/
+
+//Step 7
+/*let a = "My name is Nicole " + true;
+let b = 10 + true;
+console.log(a);
+console.log(b);*/
+
+//Step 8
+/*let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someString);*/
+
+//Step 9
+/*let a = null;
+console.log(a);
+let b;
+console.log(b);*/
+
+//Step 10
+/*let a = "The white dog";
+console.log(typeof (a));
+let b = false;
+console.log(typeof (b));
+let c = 5;
+console.log(typeof (c));
+let d = [];
+console.log(typeof (d));
+let e;
+console.log(typeof (e));*/
+
+//Step 11
+/*window.alert("Hello Zak Ruvalcaba, " + "\n" + "welcome to the JavaScript class!");*/
+
+//Step 12
+/*let name = "Nicole Nouwels";
+window.alert("Hello " + name + ", " + "\n" + "welcome to the JavaScript class!");*/
+
+//Step 13
+/*let name = "Nicole Nouwels";
+let course = "JavaScript";
+window.alert("Hello " + name + "," + "\n" + "welcome to the " + course + " class!");*/
+
+//Step 14
+//window.alert("Hello Zak Ruvalcaba. " + "\n" + "welcome to the JavaScript class!");
+
+//Step 15
+/*let name = window.prompt("Please enter your Name");
+window.alert("Hello " + name + ", " + "\n" + "welcome to the JavaScript class!");*/
+
+//Step 16
+/*
+let course = window.prompt("Enter Class Name");
+window.alert("Hello" + name + ", " + "welcome to the " + course + " class!");
+*/
+
+//Step 17
+/*let x = 10;
+let y = 20;
+window.console.log(x + y);*/
+
+//Step 18
+/*let x = 20;
+let y = x += 20;
+console.log(y);*/
+
+//Step 19
+/*let x = 20;
+let y = x *= 5;
+console.log(x);*/
+
+//Step 20
+/*let x = 20 % 3;
+let y = x /= 1;
+console.log(y);*/
+
+//Step 21
+/*let x = 5;
+let y = 10;
+if (x <= y) {
+    console.log(true);
+}*/
+//Step 22
+/*let a = "Cat";
+let b = "Horse";
+if (a.length <= b.length) {
+    console.log(false);
+}*/
+//Step 23
+/*let widget = new Object();
+console.log(typeof (widget));*/
+//Step 24
+/*let widget = new Object();
+if (widget instanceof Object) {
+    console.log(true);
+}*/
+//Step 25
+/*let widget = new Object();
+if (widget instanceof Object) {
+    console.log(false);
+}*/
